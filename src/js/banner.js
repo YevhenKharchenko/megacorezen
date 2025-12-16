0bfa27790d38ea383b434ab4b5643a3d7de91040
YevhenKharchenko <evgenvalerych@gmail.com>
2025-12-16 14:43:04 +0200

    Add cookie banner

--- src/js/banner.js
+++ src/js/banner.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const banner = document.getElementById('cookie-banner');
   const acceptBtn = document.getElementById('cookie-accept');
   const declineBtn = document.getElementById('cookie-decline');
+  const cookieCloseBtn = document.getElementById('cookie-close-btn');
 
   if (localStorage.getItem('cookieConsent')) {
     banner.classList.remove('show-cookie-banner');
@@ -16,6 +17,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   declineBtn.addEventListener('click', () => {
     localStorage.setItem('cookieConsent', 'declined');
+    banner.classList.remove('show-cookie-banner');
+  });
+
+  cookieCloseBtn.addEventListener('click', () => {
     banner.classList.remove('show-cookie-banner');
   });
 });
